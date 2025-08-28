0f80bb997729e15d98537d1b4cc9a71306144636
leninjs09 <lenin.jinkings@gmail.com>
2025-08-28 17:23:07 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -8,25 +8,35 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+	   // MOVER TORRE 5 CASAS PRA DIREIRA
+   printf("Torre cinco casas a direita\n");
+   for (int i = 0; i < 5; i++){
+       printf("Direita\n");
+   }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+   // MOVER Bispo 5 CASAS PRA DIREIRA
+   printf("Bispo 5 casas diagonal casas a direita\n");
+   for (int i = 0; i < 5; i++){
+       printf("Cima/Direita\n");
+   }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+   // MOVER rainha 8 CASAS PRA esquerda
+   printf("rainha 8 casas a esquerda\n");
+   for (int i = 0; i < 8; i++){
+       printf("Esquerda\n");
+   }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+      // movimetacao cavalo
+      printf("movimetacao cavalo\n");
+      for (int i = 1; i <= 1; i++) {
+        printf("Cima\n");
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
+          for (int y = 1; y <= 2; y++) {
+            printf("Lado\n");
+        }
+    
+    
     return 0;
 }
